14e82d5208c6ef06dc1a4e05ef2a8c6a775bb2f7
rocketup.designs <rocketup.designs@gmail.com>
2024-06-18 14:56:36 +0000

    product card component created

--- components/ui/products/most-popular-products.tsx
+++ components/ui/products/most-popular-products.tsx
@@ -1,15 +1,6 @@
-// Next js
-import Image from "next/image";
-import Link from "next/link";
-
-// Fonts
-import { small } from '@/app/fonts'
-
 // Components
 import Wrapper from "../wrapper";
 import { SimpleTitle } from "../simple-title";
-import { Button } from "../button";
-
 
 import {
     Carousel,
@@ -19,11 +10,7 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel"
 
-// Utils
-import { cn } from "@/lib/utils";
-
-// Icons
-import { Eye } from "lucide-react";
+import { ProductCard } from "./product-card";
 
 
 export function MostPopularProducts() {
@@ -53,31 +40,3 @@ export function MostPopularProducts() {
     </Wrapper>
 }
 
-function ProductCard() {
-    return <div className="relative border border-[#e8e6df] rounded-t-md rounded-b-md overflow-hidden">
-
-        <Image
-            width={1080}
-            height={1080}
-            alt=""
-            src="/images/Collection_Product-Brand-Bartlett-Pear-min.webp"
-        />
-
-        <div className="p-4 text-primary bg-white">
-            <h3 className={cn(small.className, "text-2xl mb-1")}>Barlett Pear Bundle</h3>
-            <p className={cn(small.className, "text-sm mb-8")}>Bartlett Pear Bundle is an effective skin care product that has been created as a result of extensive research into the properties of pears.</p>
-
-            <div className="flex justify-between items-center">
-                <span className={cn(small.className, "text-lg")}>$20.00</span>
-                <Button asChild variant="accent" size="sm" className="hover:bg-transparent hover:text-accent border hover:border-accent">
-                    <Link href="">
-                    <Eye className="w-4 h-4 mr-2" />
-                    Ver producto
-                    </Link>
-                </Button>
-            </div>
-        </div>
-
-
-    </div>
-}
---
+++ components/ui/products/product-card.tsx
@@ -0,0 +1,44 @@
+// Next js
+import Link from "next/link";
+import Image from "next/image";
+
+// Utils
+import { cn } from "@/lib/utils";
+
+// Fonts
+import { small } from "@/app/fonts";
+
+// Components
+import { Button } from "../button";
+
+// Icons
+import { Eye } from "lucide-react";
+
+export function ProductCard() {
+    return <div className="relative border border-[#e8e6df] rounded-t-md rounded-b-md overflow-hidden">
+
+        <Image
+            width={1080}
+            height={1080}
+            alt=""
+            src="/images/Collection_Product-Brand-Bartlett-Pear-min.webp"
+        />
+
+        <div className="p-4 text-primary bg-white">
+            <h3 className={cn(small.className, "text-2xl mb-1")}>Barlett Pear Bundle</h3>
+            <p className={cn(small.className, "text-sm mb-8")}>Bartlett Pear Bundle is an effective skin care product that has been created as a result of extensive research into the properties of pears.</p>
+
+            <div className="flex justify-between items-center">
+                <span className={cn(small.className, "text-lg")}>$20.00</span>
+                <Button asChild variant="accent" size="sm" className="hover:bg-transparent hover:text-accent border hover:border-accent">
+                    <Link href="">
+                    <Eye className="w-4 h-4 mr-2" />
+                    Ver producto
+                    </Link>
+                </Button>
+            </div>
+        </div>
+
+
+    </div>
+}
