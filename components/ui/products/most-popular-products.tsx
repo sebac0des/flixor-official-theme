9f8ad55076e7405759a7d740b1ee20aae8923ac0
rocketup.designs <rocketup.designs@gmail.com>
2024-06-18 13:43:50 +0000

    most-popular-products base carousel config

--- components/ui/products/most-popular-products.tsx
+++ components/ui/products/most-popular-products.tsx
@@ -10,14 +10,39 @@ import Wrapper from "../wrapper";
 import { Button } from "../button";
 import { SimpleTitle } from "../simple-title";
 
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+
 
 export function MostPopularProducts() {
-    return <Wrapper className="grid md:grid-cols-2 gap-5" container="stretch" >
+    return <Wrapper container="stretch" >
         <SimpleTitle
             titleText="Most Popular ✨"
             description="Looking for safe skincare products that actually work? Look no further!"
             containerClassName="text-left"
         />
+        <Carousel
+            opts={{
+                align: "start",
+                loop: true,
+            }}
+            className="w-full mt-4"
+        >
+            <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                        <ProductCard />
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
     </Wrapper>
 }
 
