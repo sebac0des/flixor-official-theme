d1155b45068cd5d578188dd6cf9b5f8f122ec69e
rocketup.designs <rocketup.designs@gmail.com>
2024-06-24 14:11:17 +0000

    product items indicator added

--- components/ui/shopping-cart.tsx
+++ components/ui/shopping-cart.tsx
@@ -1,17 +1,30 @@
 'use client'
 
+import {small} from '@/app/fonts'
+
 // Components
 import { Button } from "./button";
 
 // Icons
 import { ShoppingBasket } from "lucide-react";
 
+// Utils
+import { cn } from "@/lib/utils";
+
+
 interface HeaderCartProps {
   className?:string
 }
 
 export const ShoppingCart = (props:HeaderCartProps)=>{
-  return  <Button className={props.className} size="icon" variant='ghost' >
+  return <Button 
+  className={cn("relative",props.className)} 
+  size="icon" variant='ghost' >
+    <ProuductIndicator/>
   <ShoppingBasket/>
 </Button>
+}
+
+const ProuductIndicator = ()=>{
+  return <div className={cn(small.className,"absolute top-0 right-0 bg-soft h-5 w-5 rounded-full flex justify-center items-center text-inherit text-xs font-semibold")}>1</div>
 }
