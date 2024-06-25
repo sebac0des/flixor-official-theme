ee8a5f1096e62df55603410ad5bd26c7a0bbb847
rocketup.designs <rocketup.designs@gmail.com>
2024-06-25 18:26:27 +0000

    basic navigation added on shop page route

--- app/shop/[slug]/page.tsx
+++ app/shop/[slug]/page.tsx
@@ -1,3 +1,27 @@
+// Navigation components
+import Topbar from "@/components/topbar";
+import { Header, HeaderActionElements, HeaderButton, HeaderLogo } from "@/components/header";
+
 export default async function Page({ params }: { params: { slug: string } }) {
-    return <div>My Post: {params.slug}</div>
+    return <main>
+      <Navigation />
+    </main>
   }
+
+function Navigation() {
+    return (
+      <div className="sticky top-0 z-50">
+        <Topbar
+          url="#"
+          message="🔥 Descarga la lista de precios vigente →"
+        />
+        <Header>
+          <HeaderLogo />
+          <HeaderActionElements>
+            <HeaderButton className="hidden md:inline-block" buttonText="dsada" buttonUrl="#" />
+          </HeaderActionElements>
+        </Header>
+      </div>
+    );
+  }
+  
