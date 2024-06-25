3ad6aac3b006b58c53e8be95b6811031752348a4
rocketup.designs <rocketup.designs@gmail.com>
2024-06-25 19:40:50 +0000

    breadcum styles on shop page layout

--- app/shop/[slug]/page.tsx
+++ app/shop/[slug]/page.tsx
@@ -1,12 +1,25 @@
+// Fonts
+import {fontMono} from '@/app/fonts'
+
 // Navigation components
-import { Header, HeaderLogo } from "@/components/header"; 
-import Topbar from "@/components/topbar"; 
+import { Header, HeaderLogo } from "@/components/header";
+import Topbar from "@/components/topbar";
 
 // Components
-import { BenefitsVerticalSlider } from "@/components/ui/benefits-vertical-slider";  
+import { BenefitsVerticalSlider } from "@/components/ui/benefits-vertical-slider";
+
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
 
 // Services
-import { getBusinessBenefits } from "@/services/benefits"; 
+import { getBusinessBenefits } from "@/services/benefits";
 
 
 export default async function Page({ params }: { params: { slug: string } }) {
@@ -14,22 +27,37 @@ export default async function Page({ params }: { params: { slug: string } }) {
     const benefits = await getBusinessBenefits()
 
     return <main>
-      <Navigation />
-      <BenefitsVerticalSlider data={benefits} />
+        <Navigation />
+        <BenefitsVerticalSlider data={benefits} />
+        <Breadcrumb className={fontMono.className}>
+            <BreadcrumbList className='w-11/12 m-auto text-primary py-3 md:text-base'>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />gi
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="/components">Shop</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbPage>Producto de ejemplo</BreadcrumbPage>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+
     </main>
-  }
+}
 
 function Navigation() {
     return (
-      <div className="sticky top-0 z-50">
-        <Topbar
-          url="#"
-          message="🔥 Descarga la lista de precios vigente →"
-        />
-        <Header>
-          <HeaderLogo />
-        </Header>
-      </div>
+        <div className="sticky top-0 z-50">
+            <Topbar
+                url="#"
+                message="🔥 Descarga la lista de precios vigente →"
+            />
+            <Header>
+                <HeaderLogo />
+            </Header>
+        </div>
     );
-  }
-  
+}
