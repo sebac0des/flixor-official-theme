894719c4eae7caf18674955e8c9471493ec0db2f
rocketup.designs <rocketup.designs@gmail.com>
2024-06-25 18:21:34 +0000

    product interface moved to types/product.ts

---
+++ types/product.ts
@@ -0,0 +1,21 @@
+export interface Product {
+    id: string; 
+    name: string;
+    brand: string;
+    size: string;
+    slug:string
+    tagline:string
+    description?: string;
+    short_desc?: string;
+    audience: string;
+    price: number;
+    discount?: number;
+    ingredients?: string[];
+    cover_image: string;
+    images?: string[];
+    videos?: string[];
+    stock: number;
+    categories?: string[];
+    created_at?: Date;
+    updated_at?: Date;
+  }
