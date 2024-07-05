52188a4cb2801cef4a29334ba765cf9fdb6539ce
rocketup.designs <rocketup.designs@gmail.com>
2024-07-05 18:57:27 +0000

    benefit item refactor

--- components/flixor/benefit.tsx
+++ components/flixor/benefit.tsx
@@ -1,41 +1,38 @@
-// React
-import { ReactNode } from 'react'
+// Next js
+import Link from 'next/link'
 
 // Fonts
 import { fontMono } from '@/app/fonts'
 
 // Utils
 import { cn } from '@/lib/utils'
-import Link from 'next/link'
 
-interface Props {
-    children?: ReactNode
-    className?: string
-}
 
-function Benefit({ className, children }: Props) {
+
+
+function Benefit({ className, children }: React.HTMLAttributes<HTMLDivElement>) {
     return <div className={className}>
         {children}
     </div>
 }
 
-function BenefitItem({children,className}:Props){
+function BenefitItem({children,className}:React.HTMLAttributes<HTMLDivElement>){
     return <div className={cn("border-b border-soft pb-2",className)}>{children}</div>
 }
 
-function BenefitTitle({children}:{children:ReactNode}){
+function BenefitTitle({children}:React.HTMLAttributes<HTMLHeadingElement>){
     return <h4 className={cn(fontMono.className,"text-sm font-bold")}>{children}</h4>
 }
 
-function BenefitContent({children}:{children:ReactNode}){
+function BenefitContent({children}:React.HTMLAttributes<HTMLDivElement>){
     return <div className=''>{children}</div> 
 }
 
-function BenefitDescription({children}:{children:ReactNode}){
+function BenefitDescription({children}:React.HTMLAttributes<HTMLParagraphElement>){
     return <p className={cn("text-xs", fontMono.className)}>{children}</p> 
 }
 
-function BenefitLink({children}:Props){
+function BenefitLink({children}:React.HTMLAttributes<HTMLAnchorElement>){
     return <Link href="#" className={cn("text-xs underline", fontMono.className)}>{children}</Link> 
 }
 
