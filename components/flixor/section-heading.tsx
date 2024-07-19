a209b91ef0d9f0d4c5ebb80de50e1cf48b38db73
rocketup.designs <rocketup.designs@gmail.com>
2024-07-19 18:51:23 +0000

    section heading component refactored

--- components/flixor/section-heading.tsx
+++ components/flixor/section-heading.tsx
@@ -1,35 +1,38 @@
+import React from "react";
+
 // Fonts
 import { fontMono } from "@/app/fonts";
 
 // Utils
 import { cn } from "@/lib/utils";
-import React from "react";
 
-interface Props {
-  smallText?:string
-  titleText: string;
-  description?: string;
-  containerClassName?: string;
-}
 
-const SectionHeading = ({children,className,...props}:React.HtmlHTMLAttributes<HTMLDivElement>)=>{
-  return <div className={className} {...props}>{children}</div>
-}
 
-const SectionHeadingTitle = ({children,className,...props}:React.HtmlHTMLAttributes<HTMLHeadingElement>)=>{
-  return <h2 className="text-2xl md:text-4xl font-semibold" {...props}>{children}</h2>
-}
+const SectionHeading = React.forwardRef<HTMLDivElement,React.HTMLAttributes<HTMLDivElement>>(({className,...props},ref)=><div 
+ref={ref} 
+className={className} 
+{...props}/>)
+SectionHeading.displayName = 'SectionHeading'
 
-const SectionHeadingDescription = ({children,className,...props}:React.HtmlHTMLAttributes<HTMLParagraphElement>)=>{
-  return <p className={fontMono.className} {...props}>{children}</p>
-}
+const SectionHeadingTitle = React.forwardRef<HTMLHeadingElement,React.HTMLAttributes<HTMLHeadingElement>>(({className,...props},ref)=><h2 
+ref={ref} 
+className={cn("text-2xl md:text-4xl font-semibold",className)}
+{...props}/>)
+SectionHeadingTitle.displayName = 'SectionHeadingTitle'
 
-const SectionHeadingSmall = ({children,className,...props}:React.HtmlHTMLAttributes<HTMLElement>)=>{
-  return <small 
-  className={cn(fontMono.className,"text-inherit inline-block text-base")}
-  {...props}
-  >{children}</small>
-}
+const SectionHeadingDescription = React.forwardRef<HTMLParagraphElement,React.HTMLAttributes<HTMLParagraphElement>>(({className,...props},ref)=><p 
+ref={ref} 
+className={cn(fontMono.className,className)}
+{...props}/>)
+SectionHeadingDescription.displayName = 'SectionHeadingDescription'
+
+
+const SectionHeadingSmall = React.forwardRef<HTMLElement,React.HTMLAttributes<HTMLElement>>(({className,...props},ref)=><small 
+ref={ref} 
+className={cn(fontMono.className,"text-inherit inline-block text-base",className)}
+{...props}/>
+)
+SectionHeadingSmall.displayName = 'SectionHeadingSmall'
 
 export {
   SectionHeading,
